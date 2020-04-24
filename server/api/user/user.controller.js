a0eef9e2bb6dffec659f4c5aaea3eeb9ea881a73
Bhaktaranjan <bhaktaranjan.routaray@gmail.com>
2020-04-24 09:23:10 +0530

    Node server and API basics

--- server/api/user/user.controller.js
+++ server/api/user/user.controller.js
@@ -1,5 +1,6 @@
-const { create } = require('./user.services');
-const { genSaltSync, hashSync } = require('bcrypt');
+const { create, getUserById, getUsers, updateUser, deleteUser, getUserByEmail } = require('./user.services');
+const { genSaltSync, hashSync, compareSync } = require('bcrypt');
+const { sign } = require('jsonwebtoken');
 
 module.exports = {
     createUser: (req, res) => {
@@ -20,6 +21,111 @@ module.exports = {
                 success: 1,
                 data: results 
             })
+        });
+    },
+    getUserById: (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        getUserById(id, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    message: 'Record not found!'
+                });
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+    getUsers: (req, res) => {
+        getUsers((err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+    updateUser: (req, res) => {
+        console.log(req.body);
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+
+        updateUser(body, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return res.status(200).json({
+                success: 1,
+                message: 'User updated Successfully!' 
+            });
+        });
+    },
+    deleteUser: (req, res) => {
+        const data = req.body;
+        console.log(data);
+
+        deleteUser(data, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    message: 'Record not found!'
+                });
+            }
+            return res.json({
+                success: 1,
+                messsage: 'User deleted successfully!'
+            });
+        });
+    },
+    login: (req, res) => {
+        const data = req.body;
+        console.log(data);
+        getUserByEmail(data.email, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    message: 'Invalid email or password!'
+                });
+            }
+            console.log(results);
+            const result = compareSync(data.password, results.password);
+            if (result) {
+                results.password = undefined;
+                const jsontoken = sign({ result: results }, 'abc123', {
+                    expiresIn: '1h'    
+                });
+
+                return res.json({
+                    success: 1,
+                    message: "Login successfully!",
+                    token: jsontoken
+                });
+            } else {
+                return res.json({
+                    success: 0,
+                    message: 'Invaid Email or password!'
+                });
+            }
         });
     }
 }
--- server/api/user/user.services.js
+++ server/api/user/user.services.js
@@ -19,5 +19,76 @@ module.exports = {
                 return callback(null, results);
             }
         )
+    },
+    getUsers: callback => {
+        pool.query(
+            `Select id, first_name, last_name, email from users`,
+            [],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+
+                return callback(null, results);
+            }
+        )
+    },
+    getUserById: (id, callback) => {
+        pool.query(
+            `select id, first_name, last_name, email from users where id = ?`,
+            [id],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+
+                return callback(null, results[0]);
+            }
+        )
+    },
+    updateUser: (data, callback) => {
+        pool.query(
+            `update users SET first_name=?, last_name=?, email=?, password=? where id=?`,
+            [
+                data.first_name,
+                data.last_name,
+                data.email,
+                data.password,
+                data.id
+            ],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+
+                return callback(null, results[0]);
+            }
+        )
+    },
+    deleteUser: (data, callback) => {
+        pool.query(
+            `DELETE from users where id=?`,
+            [data.id],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+
+                return callback(null, results);
+            }
+        )
+    },
+    getUserByEmail: (email, callback) => {
+        pool.query(
+            `select id, first_name, last_name, email, password from users where email = ?`,
+            [email],
+            (error, results, fields) => {
+                if (error) {
+                    return callback(error);
+                }
+
+                return callback(null, results[0]);
+            }
+        )
     }
 }
